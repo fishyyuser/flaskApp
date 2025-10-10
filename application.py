b40d0a942e0b7d405c4648ed5c5ede2e7ddc21f7
fishyyuser <pradybisht@gmail.com>
2025-10-10 14:48:07 +0530

    ES configs

--- application.py
+++ application.py
@@ -44,4 +44,5 @@ def predict_datapoint():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    application.run(host="0.0.0.0", port=port, debug=False)
